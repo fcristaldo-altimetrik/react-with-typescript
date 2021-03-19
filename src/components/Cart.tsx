baf7ec604fd69641250e45b3298070db108d5d77
Facundo <facucristaldorodriguez@gmail.com>
2021-03-19 16:34:03 -0300

    class component with typescript

---
+++ src/components/Cart.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import classes from "./Cart.module.css";
+import { FiShoppingCart } from "react-icons/fi";
+interface Props {}
+
+interface State {
+  isOpen: boolean;
+}
+
+class Cart extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  render() {
+    return (
+      <div className={classes.cartContainer}>
+        <button
+          type="button"
+          className={classes.button}
+          onClick={() =>
+            this.setState((prevState) => ({ isOpen: !prevState.isOpen }))
+          }
+        >
+          <FiShoppingCart />
+          <span>2 pizza(s)</span>
+        </button>
+        <div
+          className={classes.cartDropDown}
+          style={{ display: this.state.isOpen ? "block" : "none" }}
+        >
+          <ul>
+            <li>Napoletana</li>
+            <li>Marinara</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Cart;
