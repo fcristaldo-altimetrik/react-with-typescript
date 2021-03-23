d9858695b777e5b212c6634052e53c3f0d8eb265
Facundo <facucristaldorodriguez@gmail.com>
2021-03-23 14:34:43 -0300

    saving data in localStorage

--- src/components/AppState.tsx
+++ src/components/AppState.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useContext, useReducer, useState } from "react";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState
+} from "react";
 
 interface AppStateValue {
   cart: {
@@ -30,9 +36,14 @@ interface AddToCartAction extends Action<"ADD_TO_CART"> {
     item: Omit<CartItem, "quantity">;
   };
 }
+interface InitializeCartAction extends Action<"INITIALIZE_CART">{
+  payload:{
+    cart:AppStateValue['cart']
+  }
+}
 
-const reducer = (state: AppStateValue, action: AddToCartAction) => {
-  if ((action.type = "ADD_TO_CART")) {
+const reducer = (state: AppStateValue, action: AddToCartAction | InitializeCartAction) => {
+  if (action.type === "ADD_TO_CART") {
     const itemToAdd = action.payload.item;
     const itemExists = state.cart.items.find(
       (item) => item.id === itemToAdd.id
@@ -51,6 +62,8 @@ const reducer = (state: AppStateValue, action: AddToCartAction) => {
           : [...state.cart.items, { ...itemToAdd, quantity: 1 }]
       }
     };
+  }else if(action.type === "INITIALIZE_CART"){
+    return {...state,cart:action.payload.cart}
   }
   return state;
 };
@@ -64,6 +77,19 @@ export const useStateDispatch = () => {
 };
 const AppStateProvider: React.FC = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, defaultState);
+
+  
+  useEffect(() => {
+    const cart = window.localStorage.getItem("cart")
+    if (cart){
+      dispatch({type:"INITIALIZE_CART",payload:{cart:JSON.parse(cart)}})
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
+
   return (
     <AppDispatchContext.Provider value={dispatch}>
       <AppStateContext.Provider value={state}>
